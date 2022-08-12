1124402d7031f3fd03db4af38b0563becfbfcccb
bryan.wu <bryan.wu@ringcentral.com>
2022-08-12 15:52:40 +0800

    111

---
+++ cert-manager/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
+kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -k ./k8s
