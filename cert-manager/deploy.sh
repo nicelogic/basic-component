19af11295c05747b53c378a258f3225988c45960
nicelogic <bryan.wu@ringcentral.com>
2022-08-31 00:13:54 +0800

    111

--- cert-manager/deploy.sh
+++ cert-manager/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
-# kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -k ./k8s
-# kubectl --kubeconfig ../devops/env-0/token/admin.conf delete -k ./k8s
+kubeConfigFilePath=$(cat ../0-env/which-env-to-apply)
+echo "current env: $kubeConfigFilePath"
 
-kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
-kubectl --kubeconfig ../devops/env-0/token/admin.conf apply -k ./k8s
+kubectl --kubeconfig $kubeConfigFilePath apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
+kubectl --kubeconfig $kubeConfigFilePath apply -k ./k8s
--- cert-manager/undeploy.sh
+++ cert-manager/undeploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+kubeConfigFilePath=$(cat ../0-env/which-env-to-apply)
+echo "current env: $kubeConfigFilePath"
 
+kubectl --kubeconfig $kubeConfigFilePath delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
+kubectl --kubeconfig $kubeConfigFilePath delete -k ./k8s
+kubectl --kubeconfig $kubeConfigFilePath delete namespace cert-manager
