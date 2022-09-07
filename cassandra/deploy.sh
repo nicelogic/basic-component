93d9a6157e2e7cc28341efcd8d856baf9488ebae
nicelogic <bryan.wu@ringcentral.com>
2022-09-07 11:12:31 +0800

    222

--- cassandra/deploy.sh
+++ cassandra/deploy.sh
@@ -3,10 +3,10 @@
 kubeConfigFilePath=$(cat ../0-env/which-env-to-apply)
 echo "current env: $kubeConfigFilePath"
 
-# helm --kubeconfig $kubeConfigFilePath repo add k8ssandra https://helm.k8ssandra.io/stable
-# helm --kubeconfig $kubeConfigFilePath repo update
+helm --kubeconfig $kubeConfigFilePath repo add k8ssandra https://helm.k8ssandra.io/stable
+helm --kubeconfig $kubeConfigFilePath repo update
 
-# helm --kubeconfig $kubeConfigFilePath install k8ssandra-operator k8ssandra/k8ssandra-operator -n k8ssandra-operator --create-namespace
+helm --kubeconfig $kubeConfigFilePath install k8ssandra-operator k8ssandra/k8ssandra-operator -n k8ssandra-operator --create-namespace
 
 kubectl --kubeconfig $kubeConfigFilePath apply -k ./k8s -n k8ssandra-operator
 
