b74a618b9751c58a23ba42891a991d177f28cbb8
nicelogic <bryan.wu@ringcentral.com>
2022-09-20 01:52:48 +0800

    traefik use yaml deploy

--- traefik/deploy.sh
+++ traefik/deploy.sh
@@ -5,10 +5,10 @@ echo "current env: $kubeConfigFilePath"
 
 # mkdir -p /opt/data/traefik/log
 
-kubectl --kubeconfig $kubeConfigFilePath apply -f ./k8s/traefik-namespace.yaml
-helm --kubeconfig $kubeConfigFilePath repo add traefik https://helm.traefik.io/traefik
-helm --kubeconfig $kubeConfigFilePath repo update
-helm --kubeconfig $kubeConfigFilePath install traefik traefik/traefik --namespace=traefik --values=./traefik-chart-values.yaml --set nodeSelector.ingresscontroller=traefik 
+# kubectl --kubeconfig $kubeConfigFilePath apply -f ./k8s/namespace.yaml
+# helm --kubeconfig $kubeConfigFilePath repo add traefik https://helm.traefik.io/traefik
+# helm --kubeconfig $kubeConfigFilePath repo update
+# helm --kubeconfig $kubeConfigFilePath install traefik traefik/traefik --namespace=traefik --values=./traefik-chart-values.yaml --set nodeSelector.ingresscontroller=traefik 
 
 kubectl --kubeconfig $kubeConfigFilePath apply -k ./k8s 
 
