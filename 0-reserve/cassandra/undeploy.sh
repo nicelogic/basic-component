fafa282bd38ef63ddf294c428a4ede098bb7295a
bryan.wu <bryan.wu@ringcentral.com>
2022-11-27 16:04:51 +0800

    remove cassnadra component

--- 0-reserve/cassandra/undeploy.sh
+++ 0-reserve/cassandra/undeploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-kubeConfigFilePath=$(cat ../0-env/which-env-to-apply)
+kubeConfigFilePath=$(cat ../../0-env/which-env-to-apply)
 echo "current env: $kubeConfigFilePath"
 
 
 
-kubectl --kubeconfig $kubeConfigFilePath delete -k ./k8s -n k8ssandra-operator
+# kubectl --kubeconfig ../$kubeConfigFilePath delete -k ./k8s -n k8ssandra-operator
+
+# helm --kubeconfig ../$kubeConfigFilePath uninstall k8ssandra-operator -n k8ssandra-operator 
+kubectl --kubeconfig ../$kubeConfigFilePath delete namespace  k8ssandra-operator
